87438d7a6eb21790201615458c3fd2165ff9664b
chrispopoola <60354263+chrispopoola@users.noreply.github.com>
2022-09-17 15:44:52 +0100

    Update SieveOfEratosthenes.java

--- Trinity College Dublin/First Year/Intermediate/SieveOfEratosthenes.java
+++ Trinity College Dublin/First Year/Intermediate/SieveOfEratosthenes.java
@@ -75,9 +75,8 @@ Comments: Yes indeed.
       Total Mark out of 100 (Add all the previous marks): 76 
 */
 package introToProgramming;
+
 import java.util.Scanner;
-
-
 import java.lang.Math;
 
 public class SieveOfEratosthenes {
@@ -86,41 +85,31 @@ public class SieveOfEratosthenes {
 	static double N;
 	static int firstPrimeNumber = 2;
 
-	public static int [] createSequence (double N) 
-	{
+	public static int [] createSequence (double N) {
 		int [] listOfAllNumbers = new int[(int) (N-1)];
 
-		if (N >= firstPrimeNumber) 
-		{
+		if (N >= firstPrimeNumber) {
 			for (int i = 0; i < N-1 ; i++ ) 
-			{
 				listOfAllNumbers [i] = firstPrimeNumber++;
-			}
 		}
 		else 
-		{
 			System.out.print("Error. Enter an integer >= 2. ");
-		}
+		
 		return listOfAllNumbers;
 	}
 
-	public static boolean [] crossOutHigherMultiples (int[] listOfNumbers) 
-	{
+	public static boolean [] crossOutHigherMultiples (int[] listOfNumbers) {
 		int [] listOfAllNumbers = new int [listOfNumbers.length];
 		System.arraycopy(listOfNumbers, 0, listOfAllNumbers, 0, listOfNumbers.length);
 
 		double squareRootOfN = Math.sqrt(N);
-
 		boolean [] testArray = new boolean [listOfAllNumbers.length];
+		
 		for (int t = 0; t < listOfAllNumbers.length; t++) 
-		{
 			testArray [t] = true;
-		}
 
-		for (int i = listOfAllNumbers.length-1; i > 0; i--) 
-		{
+		for (int i = listOfAllNumbers.length-1; i > 0; i--) {
 			for (int j = 0; listOfAllNumbers[j] <= squareRootOfN; j++) {
-
 				if (listOfAllNumbers[i] % listOfAllNumbers[j] == 0 && listOfAllNumbers[i] != listOfAllNumbers[j] )
 					testArray[i] = false;
 			}
@@ -128,76 +117,54 @@ public class SieveOfEratosthenes {
 		return testArray;
 	}
 
-	public static String [] sequenceToString (boolean[] testArray, int[] listOfNumbers) 
-	{
+	public static String [] sequenceToString (boolean[] testArray, int[] listOfNumbers) {
 		String stringArrayOfAllNumbers [] = new String[listOfNumbers.length];
 
 		for (int k = 0; k < listOfNumbers.length; k++) 
-		{
 			stringArrayOfAllNumbers[k] = String.valueOf(listOfNumbers[k]);
-		}
 
-		for (int k = 0; k < testArray.length; k++) 
-		{
+		for (int k = 0; k < testArray.length; k++) {
 			if (testArray[k] == false)
-			{
 				stringArrayOfAllNumbers [k] = "[" + listOfNumbers[k] + "]";
-			}
 		}
 		return stringArrayOfAllNumbers; 
 	}
 	
-	public static String [] nonCrossedOutSubseqToString (boolean[] booleanListOfNumbers, String [] StrListOfNumbers) 
-	{
+	public static String [] nonCrossedOutSubseqToString (boolean[] booleanListOfNumbers, String [] StrListOfNumbers) {
 		int count = 0;
 		String temporaryStringArray [] = new String [StrListOfNumbers.length];
 		System.arraycopy(StrListOfNumbers, 0, temporaryStringArray, 0, StrListOfNumbers.length);
 
-		for (int j = 0; j < booleanListOfNumbers.length; j++) 
-		{
+		for (int j = 0; j < booleanListOfNumbers.length; j++) {
 			if (booleanListOfNumbers[j] == true) 
-			{
 				count++;
-			}
 		}
+		
 		count = temporaryStringArray.length - count;
-
-		for (int i = 0; i < booleanListOfNumbers.length; i++ )
-		{
+		
+		for (int i = 0; i < booleanListOfNumbers.length; i++ ) {
 			if (booleanListOfNumbers[i] == true) 
-			{
 				System.out.print ((i <= count) ? temporaryStringArray[i] + ", " : temporaryStringArray[i] + ". ");
-			}
 		}
-
 		return temporaryStringArray;
 	}
 	
-	public static void main (String[] args) 
-	{
+	public static void main (String[] args) {
 		System.out.print("Enter an integer >= 2. ");
 		N = Input.nextDouble();
 
 		int [] listOfNumbers = createSequence (N);
 		for (int i = 0; i < N-1; i++ )
-		{
 			System.out.print ( (i < N-2) ? listOfNumbers[i] + ", " : listOfNumbers[i] + ". " + "\n");
-		}
 		
 		boolean[] booleanListOfNumbers = crossOutHigherMultiples (listOfNumbers);
-
 		String [] StrListOfNumbers = sequenceToString (booleanListOfNumbers, listOfNumbers);
 		
 		for (int i = 0; i < N-1; i++ )
-		{
 			System.out.print ((i < N-2) ? StrListOfNumbers[i] + ", " : StrListOfNumbers[i] + ". " + "\n");
-		}
 
 		nonCrossedOutSubseqToString (booleanListOfNumbers, StrListOfNumbers);
-
 		Input.close();
 	}
-
-	
 
 }
