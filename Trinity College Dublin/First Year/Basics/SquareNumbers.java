66ec7d98b99be227678336c63ee0a36d121fee7b
chrispopoola <60354263+chrispopoola@users.noreply.github.com>
2022-09-17 15:32:31 +0100

    Update SquareNumbers.java

--- Trinity College Dublin/First Year/Basics/SquareNumbers.java
+++ Trinity College Dublin/First Year/Basics/SquareNumbers.java
@@ -6,7 +6,6 @@ public class SquareNumbers {
 	public static void main(String[] args) {
 
 		Scanner Input = new Scanner (System.in);	
-
 		int squaredNumbers = 0;
 
 		System.out.print ("Enter a integer limit. ");
@@ -15,8 +14,6 @@ public class SquareNumbers {
 		for (int number = 0; number < limit; number++) {
 
 			squaredNumbers = number * number;
-
-
 			System.out.print("The numbers whose sqaures are less than " + limit + " are: ");
 			System.out.println(Math.sqrt(squaredNumbers));
 		}
