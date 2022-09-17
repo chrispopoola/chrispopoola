e9d9d92a9200ada26af20b334c51f563fafbf292
chrispopoola <60354263+chrispopoola@users.noreply.github.com>
2022-09-17 15:52:28 +0100

    Update Connect4Grid2DArray.java

--- Trinity College Dublin/First Year/Advanced/Connect - 4/Connect4Grid2DArray.java
+++ Trinity College Dublin/First Year/Advanced/Connect - 4/Connect4Grid2DArray.java
@@ -1,29 +1,25 @@
 package introToProgramming;
 
-public class Connect4Grid2DArray implements Connect4Grid
-{
+public class Connect4Grid2DArray implements Connect4Grid {
+	
 	public static final int GAME_HEIGHT = 6;
 	public static final int GAME_WIDTH = 7;
 	public static final char EMPTY_SPACE = ' ';
 	char[][] game;
 	
-	Connect4Grid2DArray()
-	{
+	Connect4Grid2DArray() {
 		game = new char[GAME_HEIGHT][GAME_WIDTH];
 		emptyGrid();
 	}
 	
 	@Override
-	public String toString()
-	{
+	public String toString() {
 		String gameString = "";
 		
-		for (int row = 0; row < GAME_HEIGHT; row++)
-		{
+		for (int row = 0; row < GAME_HEIGHT; row++) {
 			gameString = gameString + "\n|"; //puts in the borders for the columns;
 			
-			for (int column = 0; column < GAME_WIDTH; column++)
-			{
+			for (int column = 0; column < GAME_WIDTH; column++) {
 				gameString = gameString + game[row][column];
 				gameString = gameString + "|";
 			}
@@ -33,22 +29,16 @@ public class Connect4Grid2DArray implements Connect4Grid
 	}
 	
 	@Override
-	public void emptyGrid()
-	{
-		for (int row = 0; row < GAME_HEIGHT; row++)
-		{
+	public void emptyGrid() {
+		for (int row = 0; row < GAME_HEIGHT; row++) {
 			for (int column = 0; column < GAME_WIDTH; column++)
-			{
 				game[row][column] = EMPTY_SPACE;
-			}
 		}
 	}
 	
 	@Override
-	public boolean isValidColumn (int column)
-	{
-		if (column >= 0 && column < GAME_WIDTH)
-		{
+	public boolean isValidColumn (int column) {
+		if (column >= 0 && column < GAME_WIDTH) {
 			if (isColumnFull(column) == false)
 				return true;
 		}
@@ -56,10 +46,8 @@ public class Connect4Grid2DArray implements Connect4Grid
 	}
 	
 	@Override
-	public boolean isColumnFull (int column)
-	{
-		for (int row = 0; row < game.length; row++)
-		{
+	public boolean isColumnFull (int column) {
+		for (int row = 0; row < game.length; row++) {
 			if (game[row][column] == EMPTY_SPACE)
 				return false;
 		}
@@ -67,12 +55,9 @@ public class Connect4Grid2DArray implements Connect4Grid
 	}
 
 	@Override
-	public void dropPiece (ConnectPlayer player, int column)
-	{
-		for(int row = 0; row < GAME_HEIGHT; row++)
-		{
-			if (game[row][column] != EMPTY_SPACE)
-			{
+	public void dropPiece (ConnectPlayer player, int column) {
+		for(int row = 0; row < GAME_HEIGHT; row++) {
+			if (game[row][column] != EMPTY_SPACE) {
 				game[row-1][column] = player.getPiece();
 				return;
 			}
@@ -81,22 +66,18 @@ public class Connect4Grid2DArray implements Connect4Grid
 	}
 	
 	@Override
-	public boolean didLastPieceConnect4()
-	{
-		for (int row = 0; row < GAME_HEIGHT; row++)
-		{
-			for (int column = 0; column < GAME_WIDTH; column++)
-			{
+	public boolean didLastPieceConnect4() {
+		for (int row = 0; row < GAME_HEIGHT; row++) {
+			for (int column = 0; column < GAME_WIDTH; column++) {
 				char spaceToCheck = game[row][column];
 				
-				if (spaceToCheck == EMPTY_SPACE)
-				{
+				if (spaceToCheck == EMPTY_SPACE) 
 					continue;
-				}
+				
 				if (column + 3 < GAME_WIDTH && spaceToCheck == game[row][column+1] && spaceToCheck == game[row][column+2] && spaceToCheck == game[row][column+3])
 					return true;
-				if (row + 3 < GAME_HEIGHT)
-				{
+				
+				if (row + 3 < GAME_HEIGHT) {
 						if (spaceToCheck == game[row+1][column] && spaceToCheck == game[row+2][column] && spaceToCheck == game[row+3][column])
 							return true;
 						if (spaceToCheck == game[row+1][column+1] && spaceToCheck == game[row+2][column+2] && spaceToCheck == game[row+3][column+3])
@@ -110,34 +91,13 @@ public class Connect4Grid2DArray implements Connect4Grid
 	}
 	
 	@Override
-	public boolean isGridFull ()
-	{
-		for (int column = 0; column < game[0].length; column++)
-		{
+	public boolean isGridFull () {
+		for (int column = 0; column < game[0].length; column++) {
 			if (game[0][column] == EMPTY_SPACE)
 				return false;
 		}
 		return false;
-	}
-		
+	}	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
