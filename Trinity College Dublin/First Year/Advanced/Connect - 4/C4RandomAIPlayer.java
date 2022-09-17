83d6f980bc3e7b1613f8085c91729cc43b7ebfdb
chrispopoola <60354263+chrispopoola@users.noreply.github.com>
2022-09-17 15:46:59 +0100

    Update C4RandomAIPlayer.java

--- Trinity College Dublin/First Year/Advanced/Connect - 4/C4RandomAIPlayer.java
+++ Trinity College Dublin/First Year/Advanced/Connect - 4/C4RandomAIPlayer.java
@@ -2,26 +2,22 @@ package introToProgramming;
 
 import java.util.Random;
 
-public class C4RandomAIPlayer extends ConnectPlayer 
-{
-	char compPiece;
+public class C4RandomAIPlayer extends ConnectPlayer {
 	
-	public C4RandomAIPlayer (char piece)
-	{
+	char compPiece;
+	public C4RandomAIPlayer (char piece) {
 		this.compPiece = piece;
 	}
 	
 	@Override
-	public int columnToPick()
-	{
+	public int columnToPick() {
 		Random generator = new Random();
 		int column = generator.nextInt(7);
 		return column;
 	}
 
 	@Override
-	public char getPiece()
-	{
+	public char getPiece() {
 		return this.compPiece;
 	}
 }
