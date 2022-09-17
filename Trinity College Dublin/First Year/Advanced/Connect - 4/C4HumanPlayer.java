7ca3f038ba2b2cf9092e756375cc13dd4ff408a3
chrispopoola <60354263+chrispopoola@users.noreply.github.com>
2022-09-17 15:46:29 +0100

    Update C4HumanPlayer.java

--- Trinity College Dublin/First Year/Advanced/Connect - 4/C4HumanPlayer.java
+++ Trinity College Dublin/First Year/Advanced/Connect - 4/C4HumanPlayer.java
@@ -2,47 +2,44 @@ package introToProgramming;
 
 import java.util.Scanner;
 
-public class C4HumanPlayer extends ConnectPlayer 
-{
+public class C4HumanPlayer extends ConnectPlayer {
+	
 	public static final char GAME_WIDTH = 7;
 	char playerPiece;
 	static Scanner Input = new Scanner(System.in);
 	
-	public C4HumanPlayer (char piece)
-	{
+	public C4HumanPlayer (char piece) {
 		this.playerPiece = piece;
 	}
 	
 	@ Override
-	public int columnToPick()
-	{
+	public int columnToPick() {
+		
 		boolean validColumnPicked = true;
-
 		int column = 0;
 		
-		while (validColumnPicked)
-		{
+		while (validColumnPicked) {
+			
 			System.out.print("Enter a move for " + this.getPiece() + ". Remember you must enter a column number between 0-6. ");
-			if (Input.hasNextInt())
-			{
+			
+			if (Input.hasNextInt()) {
 				int columnPicked = Input.nextInt();
-				if (columnPicked >= 0 && columnPicked <= 6)
-				{
+				if (columnPicked >= 0 && columnPicked <= 6) {
 					validColumnPicked = true;
 					column = columnPicked;
 					return column;
 				}
-				else System.out.print("Invalid column number picked. Enter an column number between 0-6.");	
+				else 
+					System.out.print("Invalid column number picked. Enter an column number between 0-6.");	
 			}
-			else System.out.print("Invalid Entry. Enter an integer value between 0-6.");
+			else 
+				System.out.print("Invalid Entry. Enter an integer value between 0-6.");
 		}
 		return column;
 	}
 	
 	@Override
-	public char getPiece()
-	{
+	public char getPiece() {
 		return this.playerPiece;
-	}
-	
+	}	
 }
